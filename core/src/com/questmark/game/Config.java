b9f75ab13f5caaf20375344e74b3d2ee19f9081f
Ming Li <mingli434@gmail.com>
2018-11-21 23:28:28 -0500

    basic config stuff

---
+++ core/src/com/questmark/game/Config.java
@@ -0,0 +1,32 @@
+package com.questmark.game;
+
+/**
+ * Configuration constants for the game such as virtual screen dimensions,
+ * version, title, fps, vsync, window properties, etc.
+ *
+ * @author Ming Li
+ */
+public final class Config {
+
+    // virtual dimensions (4:3 aspect ratio)
+    public static final int V_WIDTH = 120;
+    public static final int V_HEIGHT = V_WIDTH * 3 / 4;
+    public static final int SCALE = 8;
+
+    // screen dimensions with scaling
+    public static final int S_WIDTH = V_WIDTH * SCALE;
+    public static final int S_HEIGHT = V_HEIGHT * SCALE;
+
+    public static final String NAME = "Questmark";
+    public static final String VERSION = "v0.0.1";
+    public static final String TITLE = NAME + " " + VERSION;
+
+    // performance
+    public static final int BACKGROUND_FPS = 10;
+    public static final int FOREGROUND_FPS = 60;
+
+    public static final boolean VSYNC = false;
+
+    public static final boolean RESIZABLE = false;
+
+}
--- desktop/src/com/questmark/game/desktop/DesktopLauncher.java
+++ desktop/src/com/questmark/game/desktop/DesktopLauncher.java
@@ -2,11 +2,28 @@ package com.questmark.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.questmark.game.Config;
 import com.questmark.game.Questmark;
 
+/**
+ * Main class for desktop version.
+ *
+ * @author Ming Li
+ */
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+
+	public static void main(String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.width = Config.S_WIDTH;
+		config.height = Config.S_HEIGHT;
+		config.title = Config.TITLE;
+		config.resizable = Config.RESIZABLE;
+		config.vSyncEnabled = Config.VSYNC;
+		config.backgroundFPS = Config.BACKGROUND_FPS;
+		config.foregroundFPS = Config.FOREGROUND_FPS;
+
 		new LwjglApplication(new Questmark(), config);
 	}
+
 }
