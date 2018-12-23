76c9f20e8982eaab14d96c4ef75a85f68fbae4f6
Ming Li <mingli434@gmail.com>
2018-12-22 22:40:01 -0500

    update readme

--- core/src/com/questmark/entity/components/enemy/AggressionComponent.java
+++ core/src/com/questmark/entity/components/enemy/AggressionComponent.java
@@ -5,7 +5,7 @@ import com.badlogic.ashley.core.Component;
 /**
  * A {@link Component} of an enemy entity that represents an enemy's aggression, meaning
  * if an enemy will follow and attack the player if in range. Contains a value representing
- * the radius of the effective aggression circle.
+ * the radius of the effective aggression circle and whether the entity is at its source location.
  *
  * If aggression range is -1, then the enemy has global aggression.
  *
@@ -14,9 +14,11 @@ import com.badlogic.ashley.core.Component;
 public final class AggressionComponent implements Component {
 
     public float range;
+    public boolean atSource;
 
     public AggressionComponent(float range) {
         this.range = range;
+        this.atSource = true;
     }
 
 }
